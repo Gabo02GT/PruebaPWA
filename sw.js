4be2e30f484555c1fa91bf3a172aeac1bd983983
Gabo02GT <a3522110231@alumno.uttehuacan.edu.mx>
2025-09-30 21:33:39 -0600

    Updates

---
+++ sw.js
@@ -0,0 +1,46 @@
+
+const CACHE_NAME = 'gym-tracker-v1';
+const urlsToCache = [
+  '/PruebaPWA/',
+  '/PruebaPWA/manifest.json',
+  '/PruebaPWA/icons/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Instalando...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Cacheando archivos');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activado');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Eliminando cache antigua', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
